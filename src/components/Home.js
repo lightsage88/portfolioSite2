2816463192537ca3812b489f246cee2452a502ba
Adrian <adrian.e.rosales@gmail.com>
2019-07-19 19:40:06 -0700

    got a carousel going...not sure it's the best direction, need to play with it still

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Home = () => {
+const Home = (props) => {
     const goToProjects = () => {
 
     }
@@ -9,7 +10,10 @@ const Home = () => {
             <h2>Greetings! My name is Adrian Rosales.</h2>
             <h4>I'm something of a renaissance man. I'm a professional opera singer, an amateur stand-up comic,
             and a web-developer</h4>
+            <Link to="/projects">
             <button>Take a look at my work!</button>
+
+            </Link>
         </div>
     )
 }
